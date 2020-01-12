ce9a8b14df10400539cda372265a451a5cd6b1a2
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-12 17:55:51 -0500

    all tests working

--- leapYears.js
+++ leapYears.js
@@ -1,9 +1,11 @@
 const leapYears = function(year) {
-  if (year/4 == Math.floor(year/4)) {
+  if ( year/100 == Math.floor(year/100) && year/400 == Math.floor(year/400) ) {
+    return true
+  } else if ( year/100 == Math.floor(year/100) ){
+    return false;
+  } else if ( year/4 == Math.floor(year/4) ) {
     return true;
-  }else if ( year/100 == Math.floor(year/100) && year/400 == Math.floor(year/400) ) {
-    return true;
-  }else {
+  } else{
     return false;
   }
 
